015e13abb863286d5efdaa3737788124e716a888
Robert Rypuła <robert.rypula@gmail.com>
2018-08-13 22:21:37 +0200

    Project structure update

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,5 +1,5 @@
 // Copyright (c) 2018 Robert Rypuła - https://github.com/robertrypula/simple-tetris
 
+export * from './game';
 export * from './runners/terminal-game-io-runner/terminal-game-io-runner';
-export * from './tetris';
 export * from './version';
